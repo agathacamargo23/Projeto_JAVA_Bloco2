8a855a6293168283a33ab08170ffe55f235a3c13
agathacamargo23 <agatha_rafel@hotmail.com>
2022-02-25 14:08:39 -0300

    Atualização VozAtiva2

---
+++ VozAtiva/src/main/java/br/com/VozAtiva/VozAtiva/model/TemaModel.java
@@ -0,0 +1,66 @@
+package br.com.VozAtiva.VozAtiva.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_tema")
+public class TemaModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotBlank
+	@Size(min = 3, max = 1000)
+	private String descricao;
+
+	@NotBlank
+	@Size(min = 3, max = 2000)
+	private String tipo;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+}
